cf3515b72e6dca5f1d134f860b9edf1482b7282f
Susan Adebambo Bamgbade <103441089+feistymilady01@users.noreply.github.com>
2023-03-26 23:03:40 +0100

    php

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,1 +1,10 @@
-iiiii
+# Creates a file in /tmp
+
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
